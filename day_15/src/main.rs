956acc777cd4964fac4043a61f86368cc6515534
Flippet3 <casperbekkers2@hotmail.com>
2020-12-15 15:09:35 +0100

    day15

--- day_15/src/main.rs
+++ day_15/src/main.rs
@@ -1,3 +1,51 @@
+use std::fs;
+use std::time::Instant;
+use std::collections::HashMap;
+
+fn speak_game (numbers : Vec<usize>, stop_after : usize) -> usize {
+    let mut number_hash = HashMap::with_capacity(10000);
+    let mut last_num = numbers[numbers.len()-1];
+    let mut it = numbers.len()-1;
+    let stop_after = stop_after - 1;
+    for i in 0..numbers.len()-1 {
+        number_hash.insert(numbers[i], i);
+    }
+    loop {
+        if it == stop_after {
+            break;
+        }
+        // println!("{:?}", number_hash);
+        if number_hash.contains_key(&last_num) {
+            let prev_it = number_hash[&last_num];
+            number_hash.insert(last_num, it);
+            last_num = it - prev_it;
+            it += 1;
+        } else {
+            number_hash.insert(last_num, it);
+            last_num = 0;
+            it += 1;
+        }
+    }
+    return last_num;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let test = false;
+    let filename;
+    if test {
+        filename = "data/input test.txt";
+    } else {
+        filename = "data/input.txt";
+    }
+    let contents = fs::read_to_string(filename).expect("Could not read the file");
+    let data = contents.trim().split(",").collect::<Vec<_>>();
+    let data = data.iter().map(|x| x.parse::<usize>().unwrap()).collect::<Vec<_>>();
+
+    let now = Instant::now();
+    let answer1 = speak_game(data.to_vec(), 2020);
+    let answer2 = speak_game(data, 30000000);
+    println!("{}", now.elapsed().as_secs_f64());
+
+    println!("Part 1: {}", answer1);
+    println!("Part 2: {}", answer2);
 }
